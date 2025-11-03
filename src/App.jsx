8157cb294dbb054fc17a4a1d677049c5ba20ff78
H.W.S.M.MUNASINGHE <2022t01534@stu.cmb.ac.lk>
2025-11-04 00:17:43 +0530

    Did some changes in Frontend tailwind css ✏️

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import './App.css'
-import { ProductCard } from './components/productCard'
 
 function App() {
-
-
   return (
-    <>
-    <h1>hello</h1>
-    <ProductCard name = "Laptop" price = {1000} description = "A high performance laptop" />
-    <ProductCard name = "Smartphone" price = {700} description = "A latest model smartphone" />
-    <ProductCard name = "Tablet" price = {500} description = "A lightweight tablet" />
+    <div className="border w-[600px] h-[600px] bg-gray-400 ">
+      <div className = "border w-[500px] h-[500px] bg-yellow-100 flex flex-col items-center justify-center ">
+        <div className=" w-[100px] h-[100px] bg-blue-600">
+        </div>
 
-    </>
-  )
+        <div className="w-[100px] h-[100px] bg-red-600 fixed left-[550px] top-[550px]">
+        </div>
+
+      
+        <div className="w-[100px] h-[100px] bg-green-700">
+        </div>
+
+        <div className="w-[100px] h-[100px] bg-purple-700 absolute right-[20px] bottom-[20px]">
+        </div>
+
+      </div>
+
+    </div>
+
+  );
 }
 
 export default App
